a4d9c4ce61555451e171c0ef2ed71a2d70166126
chandana M <chandu11288@gmail.com>
2018-04-17 17:47:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,33 +1,41 @@
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+    attr_accessor :name, :artist_name
+    @@all = []
 
-  def self.all
-    @@all
-  end
+    def self.all
+      @@all
+    end
 
-  def save
-    self.class.all << self
-  end
+    def save
+      self.class.all << self
+    end
 
-  def self.create
-       song =  Song.new
-       @@all << song
-       song
-  end
+    def self.create
+         song =  Song.new
+         @@all << song
+         song
+    end
 
-  def self.new_by_name(name)
-    song = Song.new
-    song.name = name
-    song
+    def self.new_by_name(name)
+      song = Song.new
+      song.name = name
+      song
+    end
+   def self.create_by_name(name)
+     song = Song.new
+     song.name = name
+      @@all << song
+     song
+   end
+   def self.find_by_name(name)
+     @@all.detect{|song| song.name == name}
   end
- def self.create_by_name(name)
-   song = Song.new
-   song.name = name
-    @@all << song
-   song
- end
- def self.find_by_name(name)
-   @@all.detect{|song| song.name == name}
-end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      name
+    else
+      song = self.create_by_name(name)
+      song
+    end
+  end
 end
